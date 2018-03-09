51d2c9ba0612e31193ff194f5ade72f59fbcabaa
Brendan Ward <bdward42@gmail.com>
2018-03-09 10:01:45 +0200

    JQuery fix

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-<script scr=""https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 $('document').ready(function(){
   $('#dropdown').hide();
   $("#menu-toggle").on("click", () => $('#dropdown').slideToggle());
